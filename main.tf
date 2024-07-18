c3c526624e0295e2f4a3cdbdae57aaad55b7832b
MaruthiMR <169152966+MaruthiMR@users.noreply.github.com>
2024-07-18 18:38:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,28 @@ resource "aws_instance" "terraform" {
     subnet_id = "subnet-03d45251a0551419b"
     
 }
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+''' variable '''
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "EC2 ami id"
+  type        = string
+  default     = "ami-12345"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my-ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
+
